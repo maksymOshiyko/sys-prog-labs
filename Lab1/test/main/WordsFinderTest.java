4f7802b174e9f7df3eac6902acfd93ea4ecd8e57
Max Oshyiko <maksym.oshiyko@gmail.com>
2021-10-09 22:57:37 +0300

    add param test

--- Lab1/test/main/WordsFinderTest.java
+++ Lab1/test/main/WordsFinderTest.java
@@ -1,11 +1,16 @@
 package main;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -59,7 +64,7 @@ class WordsFinderTest {
         int count = this.instance.getMaxConsonantsChainCount(word);
 
         // assert
-        assertEquals(5, count);
+        assertThat(5, is(count));
     }
 
     @Test
@@ -74,6 +79,19 @@ class WordsFinderTest {
         // assert
         assertNotNull(words);
         assertEquals(1, words.size());
-        assertEquals("COUNTRY", words.get(0));
+        assertThat(words,
+                contains("COUNTRY"));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ASD", "RTI", "ZXE"})
+    @DisplayName("Get max consonants chain count parametrized")
+    public void getMaxConsonantsChainCountParametrized(String arg) {
+        // act
+        int count = this.instance.getMaxConsonantsChainCount(arg);
+
+        // assert
+        assertThat(count, is(2));
+    }
+
 }
